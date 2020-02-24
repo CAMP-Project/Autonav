09e2130dd0dec35e31f87f0c6726ba37d2528bfc
tjpigott <61437822+tjpigott@users.noreply.github.com>
2020-02-24 14:58:57 -0800

    Add files via upload

---
+++ magGetter.py
@@ -0,0 +1,40 @@
+# Simple demo of the LSM9DS1 accelerometer, magnetometer, gyroscope.
+# Will print the acceleration, magnetometer, and gyroscope values every second.
+import time
+import board
+import busio
+import adafruit_lsm9ds1
+import csv
+
+# I2C connection:
+def setup():
+    i2c = busio.I2C(board.SCL, board.SDA)
+    sensor = adafruit_lsm9ds1.LSM9DS1_I2C(i2c)
+    centerX, centerY, centerZ = 0, 0, 0
+
+def getAccel():
+    return sensor.acceleration
+
+def getMag():
+    magX, magY, magZ = sensor.magnetic
+    magX = magX - centerX
+    magY = magY - centerY
+    magZ = magZ - centerZ
+    return magX, magY, magZ
+
+def getGyro():
+    return sensor.gyro
+
+def setCenterManual(x,y,z):
+    # Finds center with user given data
+    centerX = x
+    centerY = y
+    centerZ = z
+
+#def setCenter():
+#    # Finds center with fancy algorithm
+
+#main code stuff
+setup()
+setCenter(0.2,0.2)
+print getMag()
